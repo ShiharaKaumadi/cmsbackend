d4c582159316eb500060f2c4e2026f9bfb7a0a26
ShiharaKaumadi <eshani9544@gmail.com>
2024-11-28 17:24:42 +0530

    Property File Added

---
+++ src/main/java/lk/greenshadow/greens/GreensApplication.java
@@ -0,0 +1,13 @@
+package lk.greenshadow.greens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/greenshadow/greens/GreensApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.greenshadow.greens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
